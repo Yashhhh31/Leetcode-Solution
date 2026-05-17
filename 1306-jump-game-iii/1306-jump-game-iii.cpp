c959c4dc2fd0d08a30d89839bfad34220c9c4e3a
Yash Singh Parihar <yash.singhparihar31@gmail.com>
2026-05-17 17:30:51 +0530

    Time: 0 ms (100%), Space: 34.6 MB (98.06%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int n;
+
+    bool dfs(vector<int>& arr, int i){
+        if(i < 0 || i >= n || arr[i] < 0){
+            return false;
+        }
+
+        if(arr[i] == 0)
+            return true;
+
+        arr[i] *= -1;
+
+        bool a = dfs(arr,i+arr[i]);
+        bool b = dfs(arr,i-arr[i]);
+
+        return a || b; 
+    }
+
+    bool canReach(vector<int>& arr, int start) {
+        n = arr.size();
+
+        return dfs(arr,start);
+    }
+};
