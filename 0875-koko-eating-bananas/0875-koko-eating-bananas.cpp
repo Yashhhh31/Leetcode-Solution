e685dd2bcdf522cf35f11ce43291fc94fa6df753
Yash Singh Parihar <yash.singhparihar31@gmail.com>
2026-07-13 02:08:51 +0530

    Time: 15 ms (17.87%), Space: 23.1 MB (14.81%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -31,17 +31,16 @@ public:
         int r = *max_element(piles.begin(), piles.end());
         int ans = 0;
 
-        while(l <= r){
+        while(l < r){
             int mid = l + (r-l)/2;
 
             if(check(mid,piles,h)){
-                ans = mid;
-                r = mid-1;
+                r = mid;
             }else{
                 l = mid+1;
             }
         }
 
-        return ans;
+        return r;
     }
 };
