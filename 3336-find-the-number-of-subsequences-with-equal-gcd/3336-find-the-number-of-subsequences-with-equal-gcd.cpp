61754015d860855a50034f1de1f0b9056c712603
Yash Singh Parihar <yash.singhparihar31@gmail.com>
2026-07-15 04:31:14 +0530

    Time: 1107 ms (47.82%), Space: 292.2 MB (36.52%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -31,31 +31,35 @@ public:
         n = nums.size();
         int maxEl = *max_element(begin(nums), end(nums));
 
-        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(maxEl+1,vector<int>(maxEl+1, 0)));
+        vector<vector<int>>prev(maxEl+1,vector<int>(maxEl+1, 0));
 
         for(int first=0; first <= maxEl; first++){
             for(int second=0; second <= maxEl; second++){
                 bool isNotZero = (first != 0 && second != 0);
                 bool isEqual = (first == second);
 
-                dp[n][first][second] = ( isNotZero && isEqual ) ? 1 : 0; 
+                prev[first][second] = ( isNotZero && isEqual ) ? 1 : 0; 
             }
         }
 
         for(int i=n-1; i>=0; i--){
+            vector<vector<int>>curr(maxEl+1,vector<int>(maxEl+1, 0));
+
             for(int first=0; first <= maxEl; first++){
                 for(int second=0; second <= maxEl; second++){
 
-                    int skip = dp[i+1][first][second];
-                    int take1 = dp[i+1][__gcd(first,nums[i])][second];
-                    int take2 = dp[i+1][first][__gcd(second,nums[i])];
+                    int skip = prev[first][second];
+                    int take1 = prev[__gcd(first,nums[i])][second];
+                    int take2 = prev[first][__gcd(second,nums[i])];
 
-                    dp[i][first][second] = ( 0LL + skip + take1 + take2 ) % MOD;
+                    curr[first][second] = ( 0LL + skip + take1 + take2 ) % MOD;
 
                 }
             }
+
+            prev = move(curr);
         }
 
-        return dp[0][0][0];    
+        return prev[0][0];    
     }
 };
