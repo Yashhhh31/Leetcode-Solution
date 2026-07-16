36bd5897a878a12dcb2790e9cbe4dcde15d76926
Yash Singh Parihar <yash.singhparihar31@gmail.com>
2026-07-16 07:05:27 +0530

    Time: 15 ms (72.22%), Space: 23.3 MB (98.83%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-
-    typedef pair<int,int> P;
-    vector<P> direction = {{1,0}, {0,1}, {0,-1}, {-1,0}, {-1,-1}, {-1,1}, {1,-1}, {1,1}};
+    vector<pair<int,int>> direction = {
+        {1,0}, {0,1}, {0,-1}, {-1,0},
+        {-1,-1}, {-1,1}, {1,-1}, {1,1}
+    };
 
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid[0].size();
         int m = grid.size();
+        int n = grid[0].size();
 
-        if(grid[0][0] == 1) return -1;
+        if(grid[0][0] != 0 || grid[m-1][n-1] != 0) return -1;
 
-        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
-        priority_queue<P, vector<P>, greater<P>> pq;
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        grid[0][0] = 1;
 
-        pq.push({0,0});
-        result[0][0] = 1;
+        while(!q.empty()){
+            auto[r, c] = q.front();
+            q.pop();
 
-        while(!pq.empty()){
-            auto[r,c] = pq.top();
-            pq.pop();
+            if(r == m-1 && c == n-1) return grid[r][c];
 
             for(auto& dir : direction){
-                int new_r = r + dir.first;
-                int new_c = c + dir.second;
+                int nr = r + dir.first;
+                int nc = c + dir.second;
 
-                if(new_r < 0 || new_r >= m || new_c < 0 || new_c >= n || grid[new_r][new_c] == 1)
-                     continue;
+                if(nr < 0 || nr >= m || nc < 0 || nc >= n || grid[nr][nc] != 0) continue;
 
-                if(result[r][c] + 1 < result[new_r][new_c]){
-                    result[new_r][new_c] = result[r][c] + 1;
+                grid[nr][nc] = grid[r][c] + 1;
+                q.push({nr,nc});
 
-                    pq.push({new_r, new_c});
-                }
             }
         }
 
-        return result[m-1][n-1] >= INT_MAX ? -1 : result[m-1][n-1];
+        return -1;
     }
 };
