802bb4c1b17a0b7ad95f22ec027461e61e645580
Yash Singh Parihar <yash.singhparihar31@gmail.com>
2026-08-14 04:52:03 +0530

    Time: 15 ms (44.63%), Space: 30.1 MB (54.14%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -6,29 +6,35 @@ public:
         if(n % 2 != 0)
             return false;
         
-        stack<int> open, openClose;
-        
+        int open = 0;
+
         for(int i=0; i<n; i++){
-            if(locked[i] == '0'){
-                openClose.push(i);
-            }else if(s[i] == '('){
-                open.push(i);
-            }else if(s[i] == ')'){
-                if(!open.empty()){
-                    open.pop();
-                }else if(!openClose.empty()){
-                    openClose.pop();
-                }else{
-                    return false;
-                }
+            if(s[i] == '(' || locked[i] == '0'){
+                open++;
+            }else{
+                open--;
+            }
+
+            if(open < 0){
+                return false;
             }
         }
 
-        while(!open.empty() && !openClose.empty() && open.top() < openClose.top()){
-            open.pop();
-            openClose.pop();
+        int close = 0;
+
+        for(int i=n-1; i>=0; i--){
+            if(s[i] == ')' || locked[i] == '0'){
+                close++;
+            }else{
+                close--;
+            }
+
+            if(close < 0){
+                return false;
+            }
         }
 
-        return open.empty();
+
+        return true;
     }
 };
