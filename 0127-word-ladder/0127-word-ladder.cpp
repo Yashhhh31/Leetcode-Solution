37ede2b3a1e81287ae0ee9b58480d138be840338
Yash Singh Parihar <yash.singhparihar31@gmail.com>
2026-08-19 05:47:24 +0530

    Time: 167 ms (29.29%), Space: 21.5 MB (26.88%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string end, vector<string>& wordList) {
+        
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        unordered_set<string> visited;
+
+        queue<string> que;
+        que.push(beginWord);
+        visited.insert(beginWord);
+        int cnt = 1;
+
+        string alpha = "abcdefghijklmnopqrstuvwxyz";
+
+        while(!que.empty()){
+            int size = que.size();
+
+            while(size--){
+                string word = que.front();
+                que.pop();
+
+                if(word == end){
+                    return cnt;
+                }
+
+                for(char ch : alpha){
+                    for(int i=0; i<word.length(); i++){
+                        string nextWord = word;
+                        nextWord[i] = ch;
+
+                        if(!visited.count(nextWord) && st.count(nextWord)){
+                            que.push(nextWord);
+                            visited.insert(nextWord);
+                        }
+                    }
+                }
+            }
+
+            cnt++;
+        }
+
+        return 0;
+
+    }
+};
