51745430d3427197da98a200212dcc57d68f0942
Yash Singh Parihar <yash.singhparihar31@gmail.com>
2026-08-27 03:36:14 +0530

    Time: 5 ms (25.1%), Space: 12.1 MB (66.98%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    unordered_map<Node*,Node*> mp;
+
+    void dfs(Node*node, Node* cloneNode){
+
+        for(Node* v : node->neighbors){
+            if(mp.find(v) == mp.end()){
+                Node* clone = new Node(v->val);
+                cloneNode->neighbors.push_back(clone);
+                mp[v] = clone;
+
+                dfs(v, clone);
+            }else{
+                cloneNode->neighbors.push_back(mp[v]);
+            }
+        }
+
+    }
+    Node* cloneGraph(Node* node) {
+
+        if(!node){
+            return NULL;
+        }
+        
+        Node* cloneNode = new Node(node->val);
+        mp[node] = cloneNode;
+
+        dfs(node, cloneNode);
+
+        return cloneNode;
+    }
+};
