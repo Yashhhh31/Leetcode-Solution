ed2f87724c7c3feb70a635222c46c6aff7d95495
Yash Singh Parihar <yash.singhparihar31@gmail.com>
2026-08-28 04:16:29 +0530

    Time: 0 ms (100%), Space: 27.9 MB (75.56%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -8,16 +8,12 @@ public:
         if(!root)
             return 0;
 
-        int l = solve(root->left);
-        int r = solve(root->right);
+        int l = max(0,solve(root->left));
+        int r = max(0,solve(root->right));
 
-        int neeche_hi_mill_gya_answer =  l + r + root->val;
-        int koi_ek_aacha = max(l,r) + root->val;
-        int only_root_aacha = root->val;
+        maxPath = max(maxPath, l + r + root->val);
 
-        maxPath = max({maxPath, neeche_hi_mill_gya_answer, koi_ek_aacha, only_root_aacha});
-
-        return max(only_root_aacha, koi_ek_aacha);
+        return max(l,r) + root->val;
     }
 
     int maxPathSum(TreeNode* root) {
